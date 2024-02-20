d0db9cafa3ce88515d382d2695f60763033c2c5c
Andrey Shishkin <Andreishishkin99@mail.ru>
2024-02-20 19:25:15 +0400

    Создал функцию для возвращения из строки чисел

--- js/functions.js
+++ js/functions.js
@@ -18,3 +18,18 @@ const isPalindrome = (word) => {
 };
 
 const palindrome = isPalindrome('Лёша на полке клопа нашёл ');
+
+const getNumbers = (string) => {
+  if(typeof string === 'number') {
+    return;
+  }
+  let numbers = '';
+  for(let i = 0; i < string.length; i++) {
+    if(!Number.isNaN(parseInt(string[i], 10))) {
+      numbers += string[i];
+    }
+  }
+  return parseInt(numbers, 10);
+};
+
+console.log(getNumbers(-1));
